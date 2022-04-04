b7a39f9649ea0bf1309f343d5dfb8de733c3b99e
MikeSimpson1 <Petworker6@gmail.com>
2022-04-04 10:51:14 -0400

    Fixed a lot of issues, code is now runnable and cleaner

--- CRCTableSim.c
+++ CRCTableSim.c
@@ -1,59 +1,98 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//Our division polynomial.
+const unsigned int polynomial = 0x04C11DB7;
+unsigned int reverseBits(unsigned int n)
+{
+	unsigned int reversed = 0;
+
+	while (n > 0)
+	{
+		reversed  = reversed << 1;
+
+		if (n & 1)
+		{
+			reversed = reversed ^ 1;
+		}
+		n = n >> 1;
+	}
+	return reversed;
+}
 // Takes in a REVERSED input
 // Shifts the input bit by bit and applies modulo 2 addition per iteration.
 // This is meant to simulate polynomial division where calculations are done
 // in GF(2) or modulo 2 arithmetic. 
 unsigned long int compute_crc( unsigned long input, int len)
 {
-	const unsigned int polynomial = 0x04C11DB7;
-	while (len--){
-		input = (input & 0x80000000) ? polynomial ^ (input << 1) : (input << 1);
+	while (len > 0){
+		len -= 1;
+		if (input & 0x80000000)
+		{
+			input = polynomial ^ (input << 1);
+		}
+		else
+		{
+			input = input << 1;
+		}
 	}
 	return input;
 }
 
+bool isTableCreated = false;
 unsigned long crc_table[ 255 ]; // Table that stores partially calculated inputs. 
 
-unsigned long int compute_crc_WITHTABLE( unsigned char *input,
-                               int len)
+unsigned long int compute_crc_WITHTABLE( unsigned char *input, int len)
 {
-  int i;
   unsigned long crc = 0xFFFFFFFF;
 
-  for ( i = 0; i < len; i++ )
+  for (int i = 0; i < len; i++ )
   {
     crc = crc_table[ ( crc ^ input[ i ] ) & 0xff ] ^ ( crc >> 8 );
   }
 
   return crc ^ 0xFFFFFFFF;
 }
-unsigned long int crc_byte( unsigned long input,
-                            unsigned long divisor )
+unsigned long int crc_byte( unsigned long input)
 {
-  int k;
-
-  for ( k = 8; k; k-- )
+  for (int k = 8; k > 0; k-- )
   {
-    input = input & 1 ? (input >> 1) ^ divisor : input >> 1;
+	  if (input & 1)
+	  {
+		  input = (input >> 1) ^ polynomial;
+	  }
+	  else
+	  {
+		  input = input >> 1;
+	  }
   }
 
   return input;
 }
 
+bool createTable()
+{
+	if (!isTableCreated)
+	{
+		for (int i = 0; i < 255; i++ )
+		{
+			crc_table[ i ] = crc_byte( i );
+		}
+	}
+	return true;
+}
 
 int main(int argc, char *argv[]){
-	unsigned long int input = 0x8242C200; // "ABC" backwards & left aligned
-
-  printf( "%lx\n", compute_crc( input, 24)); // 5A5B433A
+	unsigned long int input = 0x8242C200; // "ABC" backwards
+	printf( "%lx\n", compute_crc( input, 24)); // 5A5B433A
   
-  // Builds the table.
-  for (int i = 0; i < 255; i++ )
-  {
-    crc_table[ i ] = crc_byte( i, 0x04C11DB7 );
-  }
 
-  printf( "%lx\n", compute_crc_WITHTABLE( input, 24)); // 5A5B433A
-  
+	printf("\n%d\n", reverseBits(input));
+	// Builds the table.
+	// Sets variable so that the table won't need to be created on
+	// subsequent runs.
+	isTableCreated = createTable();
+
+	unsigned char *input2 = "ABC";
+	printf( "%lx\n", compute_crc_WITHTABLE( input2, 24)); // 5A5B433A
 }
